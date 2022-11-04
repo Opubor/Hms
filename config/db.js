6af12c5261ef963b19a4fc16d5ebcc69709255f9
Opubor Tony <opubortony@gmail.com>
2022-11-04 08:36:53 -0700

    second commit

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require ('mongoose')
 
 async function connectDB(){
     try {
-        return await mongoose.connect('mongodb://127.0.0.1:27017/hospital')
+        return await mongoose.connect('mongodb+srv://opubor:<password>@cluster0.wuabtba.mongodb.net/?retryWrites=true&w=majority')
     } catch{
         console.log('Database Connecction Error')
     }
