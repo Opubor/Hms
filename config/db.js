94f3112dcd46758631c4bb8483c85566283cf1e1
Opubor Tony <opubortony@gmail.com>
2022-11-06 01:24:44 -0800

    Updated db password

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require ('mongoose')
 
 async function connectDB(){
     try {
-        return await mongoose.connect('mongodb+srv://opubor:Opubortony2000@cluster0.wuabtba.mongodb.net/?retryWrites=true&w=majority')
+        return await mongoose.connect('mongodb+srv://opubor:opubor@cluster0.wuabtba.mongodb.net/?retryWrites=true&w=majority')
         // return await mongoose.connect('mongodb+srv://opubor:<password>@cluster0.wuabtba.mongodb.net/?retryWrites=true&w=majority')
     } catch{
         console.log('Database Connecction Error')
