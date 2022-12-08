7fbfea000a0bf43e3867e8bc25bf3746150fb612
Opubor Tony <opubortony@gmail.com>
2022-12-08 19:11:11 +0100

    updated my cors

--- app.js
+++ app.js
@@ -6,12 +6,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors')
 
-const corsOptions ={
-  origin:'*', 
-  credentials:true,           
-  optionSuccessStatus:200,
-}
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var adminRouter = require('./routes/admin');
@@ -32,7 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors(corsOptions))
+app.use(cors())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
