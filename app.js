f7b260f038d7c2cb4b31c673870d5381dd61e8ed
Opubor Tony <opubortony@gmail.com>
2022-12-08 14:30:33 +0100

    updated cors

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors')
 
+const corsOptions ={
+  origin:'*', 
+  credentials:true,           
+  optionSuccessStatus:200,
+}
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var adminRouter = require('./routes/admin');
@@ -26,7 +32,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors())
+app.use(cors(corsOptions))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- config/db.js
+++ config/db.js
@@ -2,6 +2,7 @@ const mongoose = require ('mongoose')
 
 async function connectDB(){
     try {
+        // return await mongoose.connect('mongodb://localhost:27017/hospital')
         return await mongoose.connect('mongodb+srv://opubor:Anthonyjemi@cluster0.wuabtba.mongodb.net/?retryWrites=true&w=majority')
     } catch{
         console.log('Database Connection Error')
