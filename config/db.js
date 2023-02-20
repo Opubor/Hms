ee2c3b33d4551cd93a671554a0618a0e338eb260
Opubor Tony <opubortony@gmail.com>
2023-02-20 13:58:30 +0100

    Updated JWT

--- config/db.js
+++ config/db.js
@@ -3,7 +3,8 @@ const mongoose = require ('mongoose')
 async function connectDB(){
     try {
         // Add your database URL here EXAMPLE
-        return await mongoose.connect('mongodb://localhost:27017/hospital')
+        // return await mongoose.connect('mongodb://localhost:27017/hospital')
+        return await mongoose.connect('mongodb+srv://opubor:Anthonyjemi@cluster0.wuabtba.mongodb.net/?retryWrites=true&w=majority')
     } catch{
         console.log('Database Connection Error')
     }
